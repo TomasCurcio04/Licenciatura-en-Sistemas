34c24e675ef2da61d35b77c231f0f7c63664ca99
TomasCurcio <tomicurcio1@gmail.com>
2024-09-02 10:03:14 -0300

    Add files via upload

---
+++ SegundoAño/AyED/Resoluciones/practica 1/Ejercicio8/DoubleEndedQueue.java
@@ -0,0 +1,8 @@
+package tp1.ejercicio8;
+
+public class DoubleEndedQueue<T> extends Queue<T> {
+	
+	public void enqueueFirst(T data) {
+		super.data.add(0,data);
+	}
+}
---
+++ SegundoAño/AyED/Resoluciones/practica 1/Ejercicio8/Sequence.java
@@ -0,0 +1,9 @@
+package tp1.ejercicio8;
+
+public abstract class Sequence {
+	
+	public abstract  int size() ;
+	
+	public abstract boolean isEmpty(); 
+	
+}
