86ddd6ba5ecb6295c0d1ae24d1750a4a4652c9ca
TomasCurcio <tomicurcio1@gmail.com>
2024-09-11 10:27:18 -0300

    Add files via upload

---
+++ SegundoAño/OO1/Resoluciones/Cuadernillo de actividades/Ejercicio4/Main/Producto.java
@@ -0,0 +1,37 @@
+package ar.edu.unlp.info.oo1.Ejercicio4;
+
+public class Producto {
+	private double peso;
+	private double precioPorKilo;
+	private String descripcion;
+	
+	public double getPrecio() {
+		return this.peso*this.precioPorKilo;
+	}
+	
+	public void setPeso(double peso) {
+		this.peso=peso;
+	}
+	
+	public void setPrecioPorKilo(double precioPorKilo) {
+		this.precioPorKilo=precioPorKilo;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion=descripcion;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public double getPrecioPorKilo() {
+		return precioPorKilo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	
+}
